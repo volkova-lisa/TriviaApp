89e28343b828844be0551be4d1d8b2d2116d20e8
Your Name <liza.volk999@gmail.com>
2021-08-15 15:37:03 +0300

    CIF lesson 2

--- app/src/main/java/quizapp/volkova/triviaapp/StartingScreenActivity.java
+++ app/src/main/java/quizapp/volkova/triviaapp/StartingScreenActivity.java
@@ -2,13 +2,32 @@ package quizapp.volkova.triviaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StartingScreenActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnBeginQuiz = findViewById(R.id.begin_trivia);
+
+        btnBeginQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startTrivia();
+            }
+        });
     }
+
+    private void startTrivia() {
+        Intent intent = new Intent(StartingScreenActivity.this, QuizActivity.class);
+        startActivity(intent);
+    };
 }
